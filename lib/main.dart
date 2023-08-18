bb690615f6967d2d2517b575041e71b258b129e8
Hemanth <3842698+HemanthGowda@users.noreply.github.com>
2023-08-18 11:56:10 +0530

    configure flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:karya/pages/home.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
